7db3800f603941c26cfcec8c96436e5a22834c96
tigerFish29 <paulwchabz28@gmail.com>
2022-08-31 00:22:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Components/Header"
+
+const App = () => {
+   
+    return (
+        <>
+        <Header />
+        <div>
+            <h1>Hello World</h1>
+        </div>
+        </>
+    )
+}
+
+export default App;
